375eb56b6c329abc2c69e327e1c478577f5cf20e
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-10-09 20:35:00 +0300

    Initial Commit

---
+++ Whiskey/WhiskeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiskeyApp.swift
+//  Whiskey
+//
+//  Created by Ярослав Любиченко on 9.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WhiskeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
